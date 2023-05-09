1be5182b71afd3d71bd027e11d00c5e77f60607f
Sandesh <sandeshtidake37@gmail.com>
2023-05-09 11:46:43 +0530

    codejs deleted

--- public/teacher_feed.js
+++ public/teacher_feed.js
@@ -19,3 +19,12 @@ async function user_data() {
 }
 
 user_data();
+
+let CODE = document.getElementById("code");
+let num = 0;
+
+function randomNum() {
+  num = Math.floor(Math.random() * 10000);
+  console.log(num);
+  CODE.innerText = num;
+}
